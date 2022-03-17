68be6783ddda6289850fa9fb8f25688706b52902
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-03-17 23:46:13 +0300

    add formula cost ticket

--- src/main/java/by/asalalaiko/service/impl/JPAFlightService.java
+++ src/main/java/by/asalalaiko/service/impl/JPAFlightService.java
@@ -10,13 +10,22 @@ import by.asalalaiko.repo.TicketRepo;
 import by.asalalaiko.service.FlightService;
 import by.asalalaiko.service.PlaneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class JPAFlightService implements FlightService {
+
+    @Value("${coefficient.min.profit}")
+    private BigDecimal minProfit;
+    @Value("${coefficient.baggage}")
+    private BigDecimal cBaggage;
+    @Value("${coefficient.priority}")
+    private BigDecimal cPriority;
 
     @Autowired
     private FlightRepo flightRepo;
@@ -37,8 +46,22 @@ public class JPAFlightService implements FlightService {
     @Transactional
     public void createFlight(Flight flight) {
 
-        int seats = flight.getPlane().getPassenger_seats();
+        Integer seats = flight.getPlane().getPassenger_seats();
+        Integer km = flight.getKm();
+        BigDecimal costKm = flight.getPlane().getCost_1km();
+        BigDecimal taxStartAirport = flight.getStartAirport().getTax();
+        BigDecimal taxFinishAirport = flight.getFinishAirport().getTax();
+        BigDecimal costFlight = costKm.multiply(BigDecimal.valueOf(km)).add(taxStartAirport).add(taxFinishAirport);
+        BigDecimal costTicket = costFlight.divide(BigDecimal.valueOf(seats),2);
+        BigDecimal costBaggage = costTicket.multiply(cBaggage);
+        BigDecimal costPriority = costTicket.multiply(cPriority);
+
+        flight.setProfit(minProfit);
+        flight.setMin_ticket_cost(costTicket);
+        flight.setCostBaggage(costBaggage);
+        flight.setCostPriority(costPriority);
         flightRepo.save(flight);
+        
         for (int i=0; i<seats; i++){
             ticketRepo.save(new Ticket(flight, TicketStatus.FREE));
         }
