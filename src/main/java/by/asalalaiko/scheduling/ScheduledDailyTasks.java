9164c4af5f1f4f89e27af2105ede72a7b9310014
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-03-25 11:57:16 +0300

    Scheduled - fix bag

--- src/main/java/by/asalalaiko/scheduling/ScheduledDailyTasks.java
+++ src/main/java/by/asalalaiko/scheduling/ScheduledDailyTasks.java
@@ -3,32 +3,40 @@ package by.asalalaiko.scheduling;
 import by.asalalaiko.domain.Flight;
 import by.asalalaiko.domain.FlightStatus;
 import by.asalalaiko.service.FlightService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
+
 import java.util.List;
 
-@EnableScheduling
+@Component
 public class ScheduledDailyTasks {
 
     @Autowired
     private FlightService flightService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledDailyTasks.class);
 
 //    @Scheduled(cron = "@midnight")
-    @Scheduled(cron = "@hourly")
+//    @Scheduled(cron = "@hourly")
+    @Scheduled(cron = "0 * * * * *")
     public void  recalculationPriceFlight(){
         try {
-        List<Flight> flights = flightService.getFlightsByStatus(FlightStatus.FREE);
+            LOGGER.info("RecalculationCostTask run.");
+
+
+            List<Flight> flights = flightService.getFlightsByStatus(FlightStatus.FREE);
+
         if (flights.size() > 0) {
             flights.stream().forEach(t -> {
                 flightService.updateFlightToMidnight(t); });
         }
         } catch (Exception e) {
-//            LOGGER.error("Error running RecalculationCostTask", e);
+            LOGGER.error("Error running RecalculationCostTask", e);
         }
 
     }
